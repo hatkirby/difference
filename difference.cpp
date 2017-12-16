beb9de04047af3846bb616bfe4706c642de42602
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-12-16 15:45:38 -0500

    Fixed deadlock after failed image download

--- difference.cpp
+++ difference.cpp
@@ -281,7 +281,7 @@ Magick::Image difference::getImageAtUrl(std::string url) const
   try
   {
     imghandle.perform();
-  } catch (curl::curl_easy_exception error) {
+  } catch (const curl::curl_easy_exception& error) {
     error.print_traceback();
 
     throw could_not_get_images();
